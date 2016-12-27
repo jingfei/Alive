7f5ac0d19bab488f428016a802d7b8033b869184
maychen05 <szuche20@kas.kh.edu.tw>
2016-12-27 11:10:38 +0800

    Add search page

--- route.js
+++ route.js
@@ -9,6 +9,30 @@ route = [
     "partials": './partials.js',
     "layout":  "./view/index.hbs",
     "filename": "./docs/index.html"
+  },
+  {
+    data: {
+      "title": "Alive - search page"
+    },
+    "partials": './partials.js',
+    "layout":  "./view/searchpage.hbs",
+    "filename": "./docs/searchpage.html"
+  },
+  {
+    data: {
+      "title": "Alive - detail page"
+    },
+    "partials": './partials.js',
+    "layout":  "./view/detail.hbs",
+    "filename": "./docs/detail.html"
+  },
+  {
+    data: {
+      "title": "Alive - register page"
+    },
+    "partials": './partials.js',
+    "layout":  "./view/register.hbs",
+    "filename": "./docs/register.html"
   }
 ];
 
