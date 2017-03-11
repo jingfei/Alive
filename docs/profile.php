ca65568443ad467f289e9285880122af263a5757
bellachiu04 <bella042991@gmail.com>
2017-03-11 20:20:53 +0800

    header

---
+++ docs/profile.php
@@ -0,0 +1,72 @@
+<html><head></head><body><doctype html="">
+
+
+    <title>Alive - Apperciate Love In Volunteering Everyday</title>    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  <link rel="stylesheet" href="homepage.css">  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  <script>$('.dropdown-toggle').dropdown()</script>    <link rel="stylesheet" href="css/home.css">
+    <nav class="navbar navbar-default">  <div class="container-fluid">    <!-- Brand and toggle get grouped for better mobile display -->    <div class="navbar-header">      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">        <span class="sr-only">Toggle navigation</span>        <span class="icon-bar"></span>        <span class="icon-bar"></span>        <span class="icon-bar"></span>      </button>      <a class="navbar-brand" href="#">Alive</a>    </div>    <!-- Collect the nav links, forms, and other content for toggling -->    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      <ul class="nav navbar-nav">        <li class="active"><a href="#">Start Volunteering <span class="sr-only">(current)</span></a></li>        <li><a href="#">Search Organization</a></li>        <li><a href="#">Post a job</a></li>        <li><a href="#">Find a job</a></li>      </ul>      <ul class="nav navbar-nav navbar-right">        <li><a href="#">Login</a></li>          <li><a href="#">Register</a></li>      </ul>    </div><!-- /.navbar-collapse -->  </div><!-- /.container-fluid --></nav>  <link rel="stylesheet" href="css/Profile.css">
+
+
+  <div class="container">
+        <div class="row">
+        <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
+             <a href="edit.html">Edit Profile</a>
+        </div>
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+                <h3 class="panel-title">May Chen</h3>
+              </div>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-md-3">
+                  <div class=" col-md-9"> <img src="http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg" style="height:100px; width:100px"></div>
+                    <table class="table table-user-information">
+                      <tbody>
+                        <tr>
+                          <td>Job</td>
+                          <td>Student</td>
+                        </tr>
+                        <tr>
+                          <td>Register Date:</td>
+                          <td>01/01/2111</td>
+                        </tr>
+                        <tr>
+                          <td>Date of Birth</td>
+                          <td>01/01/2111</td>
+                        </tr>
+                           <tr>
+                               </tr><tr>
+                          <td>Gender</td>
+                          <td>Female</td>
+                        </tr>
+                          <tr>
+                          <td>Home Address</td>
+                          <td>Taiwan, Tainan</td>
+                        </tr>
+                        <tr>
+                          <td>Email</td>
+                          <td><a href="abcdefg@gmail.com"></a></td>
+                        </tr>
+                          <tr><td>Phone Number</td>
+                          <td>123-4567-890<br>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <a href="#" class="btn btn-primary">Performance</a>
+                  </div>
+                </div>
+              </div>
+                <div class="panel-footer">
+                  <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
+                  <span class="pull-right">
+                    <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+                    <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
+                  </span>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+          <div id="footer" class="row">        <div class="About col-md-3">          <h3>About</h3>          <p>About us</p>          <p>Privacy Policy</p>          <p>Customer Service</p>        </div>        <div class="Help col-md-3">          <h3>Help</h3>          <p>Help Center</p>          <p>Get Started</p>          <p>Contact us</p>        </div>        <div class="Visit-us col-md-3">          <h3>Follow Us</h3>            <img src="images/facebookicon.png" height="30px" weight="30px">            <img src="images/twitter-128.png" height="30px" weight="30px">            <img src="images/google-plus-flat.png" height="30px" weight="30px">        </div>        <div class="Contact col-md-3">          <p>Stay Connected</p>          <div class="input-group">              <input type="text" class="form-control" placeholder="Email Address">              <span class="input-group-btn">                <button class="btn btn-default" type="button">Submit</button>              </span>          </div>        </div>      </div>
+
+</doctype></body></html>
