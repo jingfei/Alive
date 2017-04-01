18a390a4c626efee18b80f9e2ddfee3426b6b793
bellachiu04 <bella042991@gmail.com>
2017-04-01 10:36:48 +0800

    postjobpage+homepage+profile

--- docs/index.php
+++ docs/index.php
@@ -2,51 +2,25 @@
     <title>Alive - Apperciate Love In Volunteering Everyday</title>    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  <link rel="stylesheet" href="homepage.css">  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  <script>$('.dropdown-toggle').dropdown()</script>    <link rel="stylesheet" href="css/home.css">
 	</head>
 	<body>
-    <nav class="navbar navbar-default">  <div class="container-fluid">    <!-- Brand and toggle get grouped for better mobile display -->    <div class="navbar-header">      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">        <span class="sr-only">Toggle navigation</span>        <span class="icon-bar"></span>        <span class="icon-bar"></span>        <span class="icon-bar"></span>      </button>      <a class="navbar-brand" href="#">Alive</a>    </div>    <!-- Collect the nav links, forms, and other content for toggling -->    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      <ul class="nav navbar-nav">        <li class="active"><a href="#">Start Volunteering <span class="sr-only">(current)</span></a></li>        <li><a href="#">Search Organization</a></li>        <li><a href="#">Post a job</a></li>        <li><a href="#">Find a job</a></li>      </ul>      <ul class="nav navbar-nav navbar-right">        <li><a href="#">Login</a></li>          <li><a href="#">Register</a></li>      </ul>    </div><!-- /.navbar-collapse -->  </div><!-- /.container-fluid --></nav>    <div id="header" class="row">
-      <div class="list">
-        <h3>List</h3>
-        <button>Most Popular</button>
-        <button>Dates</button>
-        <button>Age</button>
-      </div>
-      <div class="">
-      </div>
-      <div class="">
-      </div>
+    <nav class="navbar navbar-default">  <div class="container-fluid">    <!-- Brand and toggle get grouped for better mobile display -->    <div class="navbar-header">      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">        <span class="sr-only">Toggle navigation</span>        <span class="icon-bar"></span>        <span class="icon-bar"></span>        <span class="icon-bar"></span>      </button>      <a class="navbar-brand" href="#">Alive</a>    </div>    <!-- Collect the nav links, forms, and other content for toggling -->    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      <ul class="nav navbar-nav">        <li class="active"><a href="#">Start Volunteering <span class="sr-only">(current)</span></a></li>        <li><a href="#">Search Organization</a></li>        <li><a href="#">Post a job</a></li>        <li><a href="#">Find a job</a></li>      </ul>      <ul class="nav navbar-nav navbar-right">        <li><a href="#">Login</a></li>          <li><a href="#">Register</a></li>      </ul>    </div><!-- /.navbar-collapse -->  </div><!-- /.container-fluid --></nav>    <div class="jumbotron">
+      <h3>What are you looking for?</h3>
+<div class="style-select" tabindex="2">
     <div class="col-md-4">
-    </div>
-    <div draggable="" class="col-md-4">
-      <h4 class="title">Highlights</h4>
-      <div class="homepage">
-        <p></p><br>
-        <p></p><br>
-        <p></p><br>
-        <p></p><br>
-        <p></p><br>
-        <p></p><br>
-        <a class="btn btn-no-border share-btn-fb" onclick="window.open('https://www.facebook.com/','fb','width=626,height=436');return false;">
-  								<i class="fa fa-facebook"></i>
-                  <img src="images/facebookicon.png" height="30px" weight="30px">
-                	<span class="share-btn-text">Share</span>
-                <i class="fa fa facebook">
-                </i>
-              </a>
-                  <a class="btn btn-no-border share-btn-twitter" onclick="window.open('https://twitter.com/','tweet','width=626,height=436');return:false;">
-  								<i class="fa fa-twitter"></i>
-                  <img src="images/twitter-128.png" height="30px" weight="30px">
-  								<span class="share-btn-text">Tweet</span>
-  							</a>
-                <i class="fa fa-twitter"></i>
-              
-              <a class="btn btn-no-border share-btn-google" onclick="window.open('https://plus.google.com/?hl=zh_tw','google+','width=626,height=436');return:false;">
-  								<i class="fa fa-google-plus"></i>
-                  <img src="images/google-plus-flat.png" height="30px" weight="30px">
-  								<span class="share-btn-text">Share</span>
-  							</a>
-                <i class="fa fa-google-plus"></i>
-            </div>
-    </div>
+      <select name="search_type">
+        <option value="Volunteer Opportunities">Volunteer Opportunities</option>
+        <option value="Organization">Organization</option>
+        <option value="People">People</option>
+        <option value="Events">Events</option>
+        </select><input type="text" class="form-control" placeholder="Keywords">
+        <input type="text" class="form-control" placeholder="Where">
+        <button type="submit" class="btn btn-default">Search</button>
+     </div>
+   </div>
+   </div>
+<div class="jumbotron">
+  <div class="container">
   </div>
+</div>
           <div id="footer" class="row">        <div class="About col-md-3">          <h3>About</h3>          <p>About us</p>          <p>Privacy Policy</p>          <p>Customer Service</p>        </div>        <div class="Help col-md-3">          <h3>Help</h3>          <p>Help Center</p>          <p>Get Started</p>          <p>Contact us</p>        </div>        <div class="Visit-us col-md-3">          <h3>Follow Us</h3>            <img src="images/facebookicon.png" height="30px" weight="30px">            <img src="images/twitter-128.png" height="30px" weight="30px">            <img src="images/google-plus-flat.png" height="30px" weight="30px">        </div>        <div class="Contact col-md-3">          <p>Stay Connected</p>          <div class="input-group">              <input type="text" class="form-control" placeholder="Email Address">              <span class="input-group-btn">                <button class="btn btn-default" type="button">Submit</button>              </span>          </div>        </div>      </div>  
 
 </body></html>
