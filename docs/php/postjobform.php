35a9a2d6a9397c75634abef9287539a4167eb37c
bellachiu04 <bella042991@gmail.com>
2017-04-07 20:40:58 +0800

    postjobform php

--- docs/php/postjobform.php
+++ docs/php/postjobform.php
@@ -11,17 +11,18 @@ function handlename($data){
     $age_requirement = handlename($_POST["age_requirement"]);
     $org_name= handlename($_POST["org_name"]);
     $address= handlename($_POST["address"]);
-    $time= handlename($_POST["time"]);
+    $time= handlename($_POST["thetime"]);
     $phone_number= handlename($_POST["phone_number"]);
     $website= handlename($_POST["website"]);
     $job_description= handlename($_POST["job_description"]);
-}
-    if(empty($job_name) || empty($age_requiremnt) || empty($time) || ||empty($org_name) ||empty($address) ||empty($phone_number) ||empty($job_description)){
+
+    if(empty($job_name) || empty($age_requirement) || empty($time) || empty($org_name) ||empty($address) ||empty($phone_number) ||empty($job_description)){
       echo '<script>alert("please fill in the information");</script>';
     }
-      else {
-        $sql="INSERT INTO users (job_name,age_requirement,org_name,time,phone_number,website,job_description) VALUES('$job_name','$age_requiremnt','$org_name','$address','$time''$phone_number','$website','$job_description');";
+    else {
+      $sql="INSERT INTO jobs (userid, job_name,age_requirement,org_name,thetime,address,phone_number,website,job_description) VALUES(1,'$job_name','$age_requirement','$org_name','$address','$time','$phone_number','$website','$job_description');";
       $conn->query($sql);
   }
 
+}
 ?>
