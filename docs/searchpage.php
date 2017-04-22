b018901092f6d55470b7edb2abf89f2a6966fe5e
maychen05 <szuche20@kas.kh.edu.tw>
2017-04-22 11:53:04 +0800

    add profile setting

--- docs/searchpage.php
+++ docs/searchpage.php
@@ -1,4 +1,4 @@
-<html><head>
+<?php session_start(); ?><!DOCTYPE html><html><head>
     <title>Alive - Apperciate Love In Volunteering Everyday</title>    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  <link rel="stylesheet" href="homepage.css">  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  <script>$('.dropdown-toggle').dropdown()</script>    <link rel="stylesheet" href="css/searchpage.css">
 	</head>
 	<body>
@@ -11,6 +11,14 @@
       <button type="button" class="btn btn-secondary btn-lg">Most Recent</button>
       <button type="button" class="btn btn-secondary btn-lg">Most Views</button>
     </div>
+    <div class="col-lg-12">
+     <div class="input-group input-group-lg" style="margin:10px;">
+       <input type="text" class="form-control" name="searchvalue" method="get" placeholder="Search for...">
+       <span class="input-group-btn">
+         <button class="btn btn-default" type="button">Go!</button>
+       </span>
+     </div><!-- /input-group -->
+   </div><!-- /.col-lg-6 -->
     <div class="row">
       <div class="col-md-3 filter">
         <h2>Filter results</h2>
