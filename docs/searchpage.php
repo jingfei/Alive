f6092827ca6a4f8abaebccff8dfb7672f6040376
maychen05 <szuche20@kas.kh.edu.tw>
2017-04-29 12:06:16 +0800

    searchpage php

--- docs/searchpage.php
+++ docs/searchpage.php
@@ -2,7 +2,8 @@
     <title>Alive - Apperciate Love In Volunteering Everyday</title>    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  <link rel="stylesheet" href="homepage.css">  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  <script>$('.dropdown-toggle').dropdown()</script>    <link rel="stylesheet" href="css/searchpage.css">
 	</head>
 	<body>
-    <nav class="navbar navbar-default">  <div class="container-fluid">    <div class="navbar-header">      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">        <span class="sr-only">Toggle navigation</span>      </button>      <a class="navbar-brand" href="./index.hbs">Alive</a>    </div>    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      <ul class="nav navbar-nav">        <li class="active"><a href="#">Start Volunteering <span class="sr-only">(current)</span></a></li>        <li> <a href="">Find Organization</a></li>        <li> <a href="./searchpage.php">Find Opportunities</a></li>        <li> <a href="./postjobform.php">Post a Job</a></li>      </ul>      <?php        if(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {      ?>      <ul class="nav navbar-nav navbar-right">        <li><a href="./profile.php">My Profile</a></li>        <li><a href="./php/logout.php">Logout</a></li>      </ul>      <?php        }        else {      ?>      <ul class="nav navbar-nav navbar-right">        <li><a href="./login.php">Login</a></li>          <li><a href="./register.php">Register</a></li>      </ul>      <?php } ?>    </div>  </div></nav>    <div class="jumbotron" style="padding:10px">
+    <nav class="navbar navbar-default">  <div class="container-fluid">    <div class="navbar-header">      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">        <span class="sr-only">Toggle navigation</span>      </button>      <a class="navbar-brand" href="./index.hbs">Alive</a>    </div>    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      <ul class="nav navbar-nav">        <li class="active"><a href="#">Start Volunteering <span class="sr-only">(current)</span></a></li>        <li> <a href="">Find Organization</a></li>        <li> <a href="./searchpage.php">Find Opportunities</a></li>        <li> <a href="./postjobform.php">Post a Job</a></li>      </ul>      <?php        if(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {      ?>      <ul class="nav navbar-nav navbar-right">        <li><a href="./profile.php">My Profile</a></li>        <li><a href="./php/logout.php">Logout</a></li>      </ul>      <?php        }        else {      ?>      <ul class="nav navbar-nav navbar-right">        <li><a href="./login.php">Login</a></li>          <li><a href="./register.php">Register</a></li>      </ul>      <?php } ?>    </div>  </div></nav>    <?php @include "php/searchpage.php"; ?>
+    <div class="jumbotron" style="padding:10px">
       <h2>Search Result of ()!</h2>
       </div>
     <div class="sort-by">
@@ -13,10 +14,12 @@
     </div>
     <div class="col-lg-12">
      <div class="input-group input-group-lg" style="margin:10px;">
+       <form action="./searchpage.php" method="get">
        <input type="text" class="form-control" name="searchvalue" method="get" placeholder="Search for...">
        <span class="input-group-btn">
-         <button class="btn btn-default" type="button">Go!</button>
+           <button class="btn btn-default" type="submit">Go!</button>
        </span>
+     </form>
      </div><!-- /input-group -->
    </div><!-- /.col-lg-6 -->
     <div class="row">
@@ -104,106 +107,30 @@
       </div>
       </div>
       <div class="row col-md-9">
+<?php
+while($row=$result->fetch_assoc()){
+?>
         <div class="col-sm-6 col-md-4">
           <div class="thumbnail">
             <img src="..." alt="...">
             <div class="caption">
-              <h3>Search Result 1</h3>
-              <p>Job title: <br> Age: <br> Location: <br></p>
+              <h3>Search Result 1: <?php echo $row["job_name"];?></h3>
+              <p>Job title: <?php echo $row["job_name"];?> <br> Age:<?php echo $row["age_requirement"];?> <br> Location: <?php echo $row["address"];?><br></p>
               <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
             </div>
           </div>
         </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 2</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 3</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 4</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 5</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 6</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 7</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 8</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Search Result 9</h3>
-              <p>Job title: <br> Age: <br> Location:</p>
-              <p><a href="#" class="btn btn-primary" role="button">Click For More Info</a> <a href="#" class="btn btn-default" role="button">Save to Bookmarks</a></p>
-            </div>
-          </div>
-        </div>
-        <ul class="pagination">
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-      </ul>
-      </div>
+  <?php } ?>
+
+  <ul class="pagination">
+  <li><a href="#">1</a></li>
+  <li><a href="#">2</a></li>
+  <li><a href="#">3</a></li>
+  <li><a href="#">4</a></li>
+  <li><a href="#">5</a></li>
+  </ul>
   </div>
-  
-          <div id="footer" class="row">        <div class="About col-md-3">          <h3>About</h3>          <p>About us</p>          <p>Privacy Policy</p>          <p>Customer Service</p>        </div>        <div class="Help col-md-3">          <h3>Help</h3>          <p>Help Center</p>          <p>Get Started</p>          <p>Contact us</p>        </div>        <div class="Visit-us col-md-3">          <h3>Follow Us</h3>            <img src="images/facebookicon.png" height="30px" weight="30px">            <img src="images/twitter-128.png" height="30px" weight="30px">            <img src="images/google-plus-flat.png" height="30px" weight="30px">        </div>        <div class="Contact col-md-3">          <p>Stay Connected</p>          <div class="input-group">              <input type="text" class="form-control" placeholder="Email Address">              <span class="input-group-btn">                <button class="btn btn-default" type="button">Submit</button>              </span>          </div>        </div>      </div>  
+  </div>
+          <div id="footer" class="row">        <div class="About col-md-3">          <h3>About</h3>          <p>About us</p>          <p>Privacy Policy</p>          <p>Customer Service</p>        </div>        <div class="Help col-md-3">          <h3>Help</h3>          <p>Help Center</p>          <p>Get Started</p>          <p>Contact us</p>        </div>        <div class="Visit-us col-md-3">          <h3>Follow Us</h3>            <img src="images/facebookicon.png" height="30px" weight="30px">            <img src="images/twitter-128.png" height="30px" weight="30px">            <img src="images/google-plus-flat.png" height="30px" weight="30px">        </div>        <div class="Contact col-md-3">          <p>Stay Connected</p>          <div class="input-group">              <input type="text" class="form-control" placeholder="Email Address">              <span class="input-group-btn">                <button class="btn btn-default" type="button">Submit</button>              </span>          </div>        </div>      </div>
 
 </body></html>
--- php/searchpage.php
+++ php/searchpage.php
@@ -1,10 +1,16 @@
 <?php
-include "db.php";
-if($_SERVER[REQUEST_METHOD]==GET){
-
+@include "db.php";
+function handlename($data){
+  $data= trim($data);
+  $data= htmlspecialchars($data);
+  return $data;
+}
+$search_result= handlename($_GET["searchvalue"]);
+if($_SERVER[REQUEST_METHOD]=="GET" && $search_result.length>0){
+  $sql="SELECT * from jobs WHERE job_name LIKE '%$search_result%' or job_description LIKE '%$search_result%'";
 }
 else {
-echo '<script>alert("go away dont do this")</script>'
+$sql="SELECT * from jobs";
 }
-
+$result= $conn->query($sql);
 ?>
