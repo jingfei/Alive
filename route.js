23ce7c0ba513ae154f430d56a7f77bea766d14c5
maychen05 <szuche20@kas.kh.edu.tw>
2017-05-06 11:49:29 +0800

    index

--- route.js
+++ route.js
@@ -10,14 +10,14 @@ route = [
     "layout":  "./view/index.hbs",
     "filename": "./docs/index.php"
   },
-  {
-    data: {
-      "title": "Alive - search page"
-    },
-    "partials": './partials.js',
-    "layout":  "./view/searchpage.hbs",
-    "filename": "./docs/searchpage.php"
-  },
+  // {
+  //   data: {
+  //     "title": "Alive - search page"
+  //   },
+  //   "partials": './partials.js',
+  //   "layout":  "./view/searchpage.hbs",
+  //   "filename": "./docs/searchpage.php"
+  // },
   {
     data: {
       "title": "Alive - detail page"
@@ -58,14 +58,14 @@ route = [
     "layout":  "./view/postjob.hbs",
     "filename": "./docs/postjob.php"
   },
-  {
-    data: {
-      "title": "Alive - profile page"
-    },
-    "partials": './partials.js',
-    "layout":  "./view/profile.hbs",
-    "filename": "./docs/profile.php"
-  },
+  // {
+  //   data: {
+  //     "title": "Alive - profile page"
+  //   },
+  //   "partials": './partials.js',
+  //   "layout":  "./view/profile.hbs",
+  //   "filename": "./docs/profile.php"
+  // },
   {
     data: {
       "title": "Alive - post job form"
