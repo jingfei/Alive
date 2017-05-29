550472f4257d2b0c89c480b2ba380d202f915a51
maychen05 <szuche20@kas.kh.edu.tw>
2017-05-29 17:42:27 +0800

    fix bugs

--- docs/php/searchpage.php
+++ docs/php/searchpage.php
@@ -11,6 +11,7 @@ if($_SERVER[REQUEST_METHOD]=="GET" && strlen($search_result)>0){
   $sql="SELECT * from jobs WHERE job_name LIKE '%$search_result%' or job_description LIKE '%$search_result%'";
 } else {
   $sql="SELECT * from jobs";
+  $search_result="All Results";
 }
 $result= $conn->query($sql);
 
